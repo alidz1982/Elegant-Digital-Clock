d1335b54881bf895b0642ef364ab3617af20857f
alidz1982 <alidz1982@users.noreply.github.com>
2014-09-27 18:36:47 +0100

    Create head script.php

---
+++ head script.php
@@ -0,0 +1,4 @@
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.0.0/moment.min.js"></script>
+<script src="https://github.com/alidz1982/Elegant-Digital-Clock/blob/master/digiclock.js"></script>
+<link rel="stylesheet" type="text/css" href="https://github.com/alidz1982/Elegant-Digital-Clock/blob/master/digiclock.css">
